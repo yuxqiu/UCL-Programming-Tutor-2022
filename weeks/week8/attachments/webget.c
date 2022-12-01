a7acd50ee4e6d420eca30030e41efc6c2660d2c8
Tim Qiu <qyxtim@qq.com>
2022-12-01 18:21:12 +0000

    feat: add week8 materials

---
+++ weeks/week8/attachments/webget.c
@@ -0,0 +1,129 @@
+#include <netdb.h> // for addrinfo
+#include <stdio.h> // for fprintf, printf
+#include <stdlib.h> // for exit, EXIT_FAILURE, EXIT_SUCCESS
+#include <string.h> // for strlen
+#include <sys/errno.h> // for errno and strerrno
+#include <sys/socket.h> // for socket and connect
+#include <unistd.h> // for close
+
+#define BUFFER_SIZE 4096
+
+void GetPage(const char *host, const char *path);
+
+int main(const int argc, const char *argv[]) {
+  if (argc != 3) {
+    printf("Usage: %s hostname path\n", argv[0]);
+    printf("Example: %s www.ucl.ac.uk /\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  const char *host = argv[1];
+  const char *path = argv[2];
+  GetPage(host, path);
+  return EXIT_SUCCESS;
+}
+
+// Adopted from CMU 15213
+void Getaddrinfo(const char *node, const char *service,
+                 const struct addrinfo *hints, struct addrinfo **res) {
+  int rc;
+  if ((rc = getaddrinfo(node, service, hints, res)) != 0) {
+    fprintf(stderr, "Getaddrinfo: %s\n", gai_strerror(rc));
+    exit(EXIT_FAILURE);
+  }
+}
+
+// Adopted from CMU 15213
+void Close(int fd) {
+  int rc;
+
+  if ((rc = close(fd)) < 0) {
+    fprintf(stderr, "Close: %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+}
+
+// Adopted from CMU 15213
+int GetClientFd(const char *hostname, const char *port) {
+  int clientfd;
+  struct addrinfo hints = {0}, *listp, *p;
+
+  /* Get a list of potential server addresses */
+  hints.ai_socktype = SOCK_STREAM; /* Open a connection */
+  hints.ai_flags = AI_NUMERICSERV; /* ... using a numeric port arg. */
+  hints.ai_flags |= AI_ADDRCONFIG; /* Recommended for connections */
+  Getaddrinfo(hostname, port, &hints, &listp);
+
+  /* Walk the list for one that we can successfully connect to */
+  for (p = listp; p; p = p->ai_next) {
+    /* Create a socket descriptor */
+    if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+      continue; /* Socket failed, try the next */
+
+    /* Connect to the server */
+    if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
+      break; /* Success */
+    Close(clientfd);
+    /* Connect failed, try another */ // line:netp:openclientfd:closefd
+  }
+
+  /* Clean up */
+  freeaddrinfo(listp);
+  if (!p) /* All connects failed */
+    return -1;
+  else /* The last connect succeeded */
+    return clientfd;
+}
+
+// Never use this code in reality
+// The return value of write needs to be checked to ensure content is
+// successfully written
+void SendRequest(int fd, const char *host, const char *path) {
+  const char *request_line_one_start = "GET ";
+  const char *request_line_one_end = " HTTP/1.1\r\n";
+  const char *request_line_two_start = "Host: ";
+  const char *request_end = "\r\nConnection: close\r\n\r\n";
+
+  write(fd, request_line_one_start, strlen(request_line_one_start));
+  write(fd, path, strlen(path));
+  write(fd, request_line_one_end, strlen(request_line_one_end));
+  write(fd, request_line_two_start, strlen(request_line_two_start));
+  write(fd, host, strlen(host));
+  write(fd, request_end, strlen(request_end));
+}
+
+// Get Response from Fd
+// Return null if all the content is read
+//
+// Very Inefficient
+// and Return NULL should be considered as a bad design?
+char *GetResponse(int fd) {
+  static char buffer[BUFFER_SIZE];
+  ssize_t rc = read(fd, buffer, BUFFER_SIZE - 1);
+  if (rc > 0) {
+    buffer[rc] = '\0';
+    return buffer;
+  } else if (rc < 0) {
+    // neglect EINTR here for convenience
+    // shouldn't do this in real world
+    fprintf(stderr, "GetResponse: %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  return NULL;
+}
+
+void GetPage(const char *host, const char *path) {
+  int client_fd = GetClientFd(host, "http");
+  if (client_fd == -1) {
+    fputs("GetPage: failed to get client fd", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  SendRequest(client_fd, host, path);
+  char *content;
+  while ((content = GetResponse(client_fd)) != NULL) {
+    puts(content);
+  }
+
+  Close(client_fd);
+}
---
+++ weeks/week8/note.md
@@ -0,0 +1,38 @@
+# Week8
+
+## Network
+
+1. [How the Internet works?](https://cs144.github.io/handouts/week-1-how-the-internet-works.pdf)
+2. Demo: `ping yuba.stanford.edu`
+3. `telnet`
+
+
+## Layering
+
+1. [Layering](https://github.com/khanhnamle1994/computer-networking/blob/master/Unit1-Internet-and-IP/1-2-four_layers.pdf)
+2. Four Layer Model
+    - TCP and UDP
+3. OSI Model
+
+
+## From Top to Bottom
+
+1. Wireshark
+2. Ethernet
+    - MAC address
+    - ARP
+
+
+## Back to Top (Application)
+
+1. Socket
+2. Server
+    - `socket`
+    - `bind`
+    - `listen`
+    - `accept`
+3. Client
+    - `socket`
+    - `connect`
+4. Get address
+    - `getaddrinfo`
